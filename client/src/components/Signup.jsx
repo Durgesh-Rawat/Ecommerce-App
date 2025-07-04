2481d27ea13d0548c4d465e6a03265a60f91803f
Durgesh Rawat <durgeshrawat9046@gmail.com>
2025-07-04 10:10:30 +0530

    Update Signup.jsx

--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -11,7 +11,7 @@ function Signup(){
     const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const res = await fetch('http://localhost:5000/api/auth/signup', {
+    const res = await fetch('https://ecommerce-app-9is1.onrender.com/api/auth/signup', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json',
                   'Authorization': `Bearer ${token}`,
